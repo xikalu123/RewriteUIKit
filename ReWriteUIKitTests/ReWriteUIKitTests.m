b3b34b1b57e985214c65cf160d296d183861582e
chenyuliang <chenyuliang@didichuxing.com>
2018-04-08 14:20:39 +0800

    UITableView

---
+++ ReWriteUIKitTests/ReWriteUIKitTests.m
@@ -0,0 +1,39 @@
+//
+//  ReWriteUIKitTests.m
+//  ReWriteUIKitTests
+//
+//  Created by didi on 2018/4/8.
+//  Copyright © 2018年 devil. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ReWriteUIKitTests : XCTestCase
+
+@end
+
+@implementation ReWriteUIKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
